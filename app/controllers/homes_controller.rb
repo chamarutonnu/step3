392d07ecbf8a8d63eb892630d4029a86b81865c5
vagrant <vagrant@localhost.localdomain>
2020-08-27 10:29:37 +0000

    [complete]

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,6 @@
 class HomesController < ApplicationController
   def top
     @famous_users = User.where(id: User.joins(:post_images).group(:id).count("post_images.id").sort {|a,b| a[1]<=>b[1]}.last(5).map{|x| x.first}.last(5))
-    @post_image = PostImage.find(34)
   end
 
   def about
